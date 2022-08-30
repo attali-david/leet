17c9c05d40e581f43cd03ff88fb18ebbc1cc959a
attali-david <dattali93@gmail.com>
2022-08-30 10:43:29 -0500

    small correction to replacement file

--- longestRepeatingReplacement.js
+++ longestRepeatingReplacement.js
@@ -1,32 +1,20 @@
 var characterReplacement = function(s, k) {
-    let left = 0
-    let len = 0
-    let hash = {}
-    let temp = k
-    hash[s[left]] = 1
+    let l = 0
+    let maxF = 0
+    let map = {}
+    let res = 0
     
-    for(let right = 1; right < s.length; right++) {
-        console.log('START', s[right], hash)
-        if(hash[s[right]]) hash[s[right]] = hash[s[right]] + 1
-        while(!hash[s[right]] && temp > 0) {
-            console.log('while')
-            temp--
-            hash[s[left]] = hash[s[left]] + 1
-            len = Math.max(len, hash[s[left]])
-            continue 
+    for(let r = 0; r < s.length; r++) {
+        map[s[r]] = map[s[r]] ? map[s[r]] + 1 : 1
+        maxF = Math.max(maxF, map[s[r]])
+        
+        while((r - l + 1 - maxF) > k) {
+            map[s[l]] = map[s[l]] - 1
+            l++
         }
-        len = Math.max(len, hash[s[left]])
-        if(!hash[s[right]] && temp === 0) {
-            left = right - k
-            right = left + 1
-            hash = {}
-            console.log('TEST', hash)
-            hash[s[left]] = 1
-        }
-        console.log('END', hash)
+        res = Math.max(res, r - l + 1)
     }
-    console.log(len)
-    return len
+    return res
 };
 
-characterReplacement("AABABBA", 1)
+// optimal solutionS
