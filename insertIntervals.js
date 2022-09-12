ab386660ec67fe1f8f3cfd0bd51d37b0807800b7
attali-david <dattali93@gmail.com>
2022-09-12 10:28:59 -0500

    fixed insert

--- insertIntervals.js
+++ insertIntervals.js
@@ -1,21 +1,18 @@
 var insert = function(intervals, newInterval) {
     let res = []
+    
     for(let i = 0; i < intervals.length; i++) {
         let int = intervals[i]
         if(newInterval[1] < int[0]) {
             res.push(newInterval, ...intervals.splice(i))
-            console.log(res)
             return res
         } else if (newInterval[0] > int[1]) {
             res.push(int)
         } else {
-            console.log('test', newInterval)
             newInterval = [Math.min(int[0], newInterval[0]), Math.max(int[1], newInterval[1])]
-            console.log('here', newInterval)
         }
     }
     res.push(newInterval)
-    console.log(res)
     return res
 };
 
